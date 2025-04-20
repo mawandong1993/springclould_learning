73f928268badd749cc6251ad0096c4a8c9dfbf55
mawandong1993 <mawandong1993@foxmail.com>
2025-04-20 14:09:12 +0800

    Default Changelist

--- eureka-client-02/src/main/java/com/learning/eurekaclient02/EurekaClient02Application.java
+++ eureka-client-02/src/main/java/com/learning/eurekaclient02/EurekaClient02Application.java
@@ -1,4 +1,4 @@
-package com.learning.eurekaclient01;
+package com.learning.eurekaclient02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,9 +8,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-public class EurekaClient01Application {
+public class EurekaClient02Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaClient01Application.class, args);
+        SpringApplication.run(EurekaClient02Application.class, args);
     }
 }
--- eureka-server/src/main/java/com/learning/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/learning/eurekaserver/EurekaServerApplication.java
@@ -2,10 +2,8 @@ package com.learning.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableConfigServer
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
